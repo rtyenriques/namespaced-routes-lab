f85b6c34f864333ea598fcc7879bf3e6d0d0eb54
= <rtyenriques@gmail.com>
2021-10-06 23:58:40 -0700

    Done.

---
+++ app/models/preference.rb
@@ -0,0 +1,4 @@
+class Preference < ApplicationRecord
+
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :artists do
     resources :songs, only: [:index, :show]
   end
-  resources :songs
+  resources :songs 
+  namespace :admin do
+    resources :preferences
+  end
 
 end
